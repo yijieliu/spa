ed7d9f6dd3a4afe3f6095cb3af11dd32a47fdb1b
yijieliu <951014453@qq.com>
2018-03-08 16:40:55 +0800

    add new1

--- 01-first-app/rectangle.js
+++ 01-first-app/rectangle.js
@@ -21,6 +21,12 @@ $(function(){
         lenY=(y.split('.').length===2)? y.split('.')[1].length:0;
 
     var max= Math.max(lenX,lenY);
+    return (x*(10**max)+y*(10**max))*2/(10**max);
+  }
+  function area(x,y){
+    var lenX=(x.split('.').length===2)? x.split('.')[1].length:0,
+        lenY=(y.split('.').length===2)? y.split('.')[1].length:0;
+    var max= Math.max(lenX,lenY);
     return (x*(10 ** max))*(y*(10 ** max))/(10 ** (2*max));
   }
 
